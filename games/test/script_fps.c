7dbfeb42b526f3a4a38faecf7b2d2034276e1607
Phil Desktop <philipp033.25@gmail.com>
2024-02-19 02:23:07 +0100

    DESKTOP: from fisch, fps hitscan shooting, player ui

--- games/test/script_fps.c
+++ games/test/script_fps.c
@@ -1,3 +1,4 @@
+#include "core/types/entity.h"
 #include "test/scripts.h"
 #include "test/entity_tags.h"
 #include "test/entity_table.h"
@@ -8,10 +9,12 @@
 #include "core/io/save_sys/save_sys.h"
 #include "core/camera.h"
 #include "core/state/state.h"
+#include "core/io/assetm.h"
 #include "core/debug/debug_draw.h"
 
 #include "math/math_inc.h"
 #include "phys/phys_ray.h"  // raycasting
+#include "mui/mui.h"
 
 #include "games.h"  // includes bool SCRIPT_REMOVE_FUNC_GENERIC_NAME(u32 uid);
 
@@ -29,7 +32,8 @@ static vec3 start_pos = { 0, 0, 0 }; // starting position of player char
 static int ammo_count = AMMO_MAX;
 
 // --- func-decls ---
-void script_fps_cam(entity_t* this);
+static void script_fps_cam(entity_t* this);
+static void scritp_fps_ui(entity_t* this);
 
 void SCRIPT_REGISTER_TRIGGER_CALLBACK_FUNC(fps_controller_script_t)  
 {
@@ -63,6 +67,8 @@ void SCRIPT_UPDATE(fps_controller_script_t)
   core_data = core_data_get();
   entity_t* this = state_entity_get(script->entity_id);
   f32 dt = core_data->delta_t;
+
+  scritp_fps_ui(this);
 
   //  @NOTE: moving object with physics
   f32 speed      = 500.0f * dt;
@@ -140,6 +146,12 @@ void SCRIPT_UPDATE(fps_controller_script_t)
       if ( phys_ray_cast(&ray, &hit) )
       {
         // PF("from cam hit: "); P_INT(hit.entity_idx);
+        entity_t* e = state_entity_get(hit.entity_idx);
+        vec3 f;
+        vec3_sub(hit.hit_point, this->pos, f);
+        vec3_normalize(f, f);
+        vec3_mul_f(f, 200.0f, f);
+        ENTITY_FORCE(e, f);
       }
     }
   }
@@ -150,7 +162,7 @@ void SCRIPT_UPDATE(fps_controller_script_t)
   }
 }
 
-void script_fps_cam(entity_t* this)
+static void script_fps_cam(entity_t* this)
 {
   // set cam pos to player pos
   vec3_copy(VEC3_XYZ(this->pos[0],
@@ -224,14 +236,88 @@ void script_fps_cam(entity_t* this)
     // rot[0] += 15;  // sway or something
     // rot[0] += (f32)sin(core_data->total_t) * 90.0f;
     rot[0] = pitch + 25;
-    // rot[1] = this->rot[1];
+    rot[1] = this->rot[1];
     mat4_make_model(core_data->cam.pos, rot, VEC3(1), rot_mat);  
     mat4_mul(rot_mat, e->model, e->model);
-    f32 rot_y_rad = this->rot[1]; 
+    // f32 rot_y_rad = this->rot[1]; 
     // m_deg_to_rad(&rot_y_rad);
-    // mat4_rotate_make(rot_mat, rot_y_rad, VEC3_XYZ(0, 1, 0));
-    mat4_mul(rot_mat, e->model, e->model);
+    // vec3 axis;
+    // // camera_get_right_axis(axis);
+    // // camera_get_up_axis(axis);
+    // // vec3_mul_f(core_data->cam.front, -1.0f, axis);
+    // vec3_copy(core_data->cam.front, axis);
+    // mat4_rotate_make(rot_mat, rot_y_rad, axis);
+    // mat4_mul(rot_mat, e->model, e->model);
     
     e->skip_model_update = true;  // explicitly not update model, cause we do it here
+  }
+}
+
+static void scritp_fps_ui(entity_t* this)
+{
+  texture_t* circle_tex = assetm_get_texture("#internal/circle.png", false);
+  texture_t* weapon_tex = assetm_get_texture("_icons/kriss_vector_01.png", false);
+  
+  // reticle
+  mui_img_tint(VEC2_XY(0.0f, 0.0f),   VEC2(0.05f), circle_tex, VEC3(1.0f));
+  
+  // -- circle & ammo --
+  { 
+    mui_img_tint(VEC2_XY(-0.8f, -0.8f),   VEC2(0.80f), circle_tex, VEC3(0.75f));
+    mui_img_tint(VEC2_XY(-0.8f, -0.8f),   VEC2(0.65f), circle_tex, VEC3(0.55f));
+  
+    mui_img_tint(VEC2_XY(-0.8f, -0.8f),   VEC2(0.45f), weapon_tex, VEC3(1.00f));
+  
+    mui_img_tint(VEC2_XY(-0.72f, -0.72f), VEC2(0.50f), circle_tex, VEC3(0.35f));
+  
+    char txt[64];
+    SPRINTF(64, txt, "%d|%d", ammo_count, AMMO_MAX);
+    mui_text(VEC2_XY(-0.70f, -0.65f), txt, MUI_CENTER | MUI_UP);
+  }
+  // -- inventory --
+  {
+    mui_group_t group;
+    mui_group_t items;
+    // MUI_GROUP_T_INIT(&group, VEC2_XY(0.0f, -0.8f), VEC2_XY(1.75f, 0.5f), 0.0f, MUI_DYNAMIC | MUI_CENTER | MUI_ROW, false);
+    // MUI_GROUP_T_INIT(&items, VEC2_XY(0.0f, -0.8f), VEC2_XY(1.75f, 0.5f), 0.0f, MUI_DYNAMIC | MUI_CENTER | MUI_ROW, false);
+    // 
+    // MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 1.00f, 1.00f, 1.00f));
+    // MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.75f, 0.75f, 0.75f));
+    // MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.50f, 0.50f, 0.50f));
+    // MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.25f, 0.25f, 0.25f));
+    // 
+    // mui_group(&group);
+    {
+      MUI_GROUP_T_INIT(&group, VEC2_XY(0.0f, -0.8f), VEC2_XY(1.75f, 0.5f), 0.0f, MUI_STATIC | MUI_CENTER | MUI_ROW, false);
+      
+      MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 1.00f, 1.00f, 1.00f));
+      MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.75f, 0.75f, 0.75f));
+      MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.50f, 0.50f, 0.50f));
+      MUI_GROUP_T_ADD(&group, MUI_OBJ_T_IMG_GROUP(circle_tex, 0.25f, 0.25f, 0.25f));
+      
+      mui_group(&group);
+    }
+    {
+      MUI_GROUP_T_INIT(&items, VEC2_XY(0.0f, -0.8f), VEC2_XY(1.75f, 0.5f), 0.0f, MUI_STATIC | MUI_CENTER | MUI_ROW, false);
+      mui_obj_t* slot_1, *slot_2, *slot_3, *slot_4;
+      MUI_GROUP_T_ADD_PTR(&items, MUI_OBJ_T_IMG(0.0f, 0.0f, 0.55f, 0.55f, weapon_tex, 1.00f, 1.00f, 1.00f), slot_1);
+      MUI_GROUP_T_ADD_PTR(&items, MUI_OBJ_T_IMG(0.0f, 0.0f, 0.55f, 0.55f, weapon_tex, 0.75f, 0.75f, 0.75f), slot_2);
+      MUI_GROUP_T_ADD_PTR(&items, MUI_OBJ_T_IMG(0.0f, 0.0f, 0.55f, 0.55f, weapon_tex, 0.50f, 0.50f, 0.50f), slot_3);
+      MUI_GROUP_T_ADD_PTR(&items, MUI_OBJ_T_IMG(0.0f, 0.0f, 0.55f, 0.55f, weapon_tex, 0.25f, 0.25f, 0.25f), slot_4);
+      
+      static int active_slot = 1;
+      slot_1->active = active_slot == 1;
+      slot_2->active = active_slot == 2;
+      slot_3->active = active_slot == 3;
+      slot_4->active = active_slot == 4;
+  
+      if (input_get_key_pressed(KEY_ALPHA1) || input_get_key_pressed(KEY_NUMPAD1)) { active_slot = 1; }
+      if (input_get_key_pressed(KEY_ALPHA2) || input_get_key_pressed(KEY_NUMPAD2)) { active_slot = 2; }
+      if (input_get_key_pressed(KEY_ALPHA3) || input_get_key_pressed(KEY_NUMPAD3)) { active_slot = 3; }
+      if (input_get_key_pressed(KEY_ALPHA4) || input_get_key_pressed(KEY_NUMPAD4)) { active_slot = 4; }
+  
+      
+      mui_group(&items);
+    }
   }
 }
