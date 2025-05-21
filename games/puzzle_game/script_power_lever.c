5e51fe3e99a2ea116744c4964ade20698a4b5024
Phil <philipp033.25@gmail.com>
2025-05-21 20:30:13 +0200

    DESKTOP: added sound to lever

--- games/puzzle_game/script_power_lever.c
+++ games/puzzle_game/script_power_lever.c
@@ -28,6 +28,7 @@
 
 
 const f32 turn_t_max = 1.0f;
+static u32 sound_lever_idx = SOUND_INVALID_IDX;
 
 
 void SCRIPT_INIT(power_lever_script_t)
@@ -35,6 +36,8 @@ void SCRIPT_INIT(power_lever_script_t)
   entity_t* this = state_entity_get(script->entity_id);
   ENTITY_SET_ROT_X( this, 90.0f );
   script->turn_t = turn_t_max;
+
+  sound_lever_idx = audio_load_clip("lever_01.mp3", SOUND_SPATIAL);  
 }
 void SCRIPT_CLEANUP(power_lever_script_t)
 {
@@ -65,10 +68,15 @@ void power_lever_script_t_set_activated( power_lever_script_t* script, bool act 
 {
   script->activated = act;
   script->turn_t = 0.0f;
+ 
   bool err = false;
   entity_t* e = state_entity_get(script->entity_id);
   point_light_t* pl = state_point_light_get( e->point_light_idx, &err);
   if (err) { P_ERR("failed getting pointlight"); return; }
   vec3_copy(act ? VEC3_XYZ(0, 1, 0) : VEC3_XYZ(1, 0, 0), pl->color);
+
+  vec3 e_pos;
+  mat4_get_pos( e->model, e_pos );
+  audio_play_sound_spatial(sound_lever_idx, 20.0f, e_pos);
 }
 
